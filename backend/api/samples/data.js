42a1e7f30f5a92c3790b02de856238f2a9336dfa
kartikpro9599-hash <kartikpro9599@gmail.com>
2026-09-20 18:49:24 +0530

    add a example data

---
+++ backend/api/samples/data.js
@@ -0,0 +1,47 @@
+export const exampleData = [
+  {
+    coord: {
+      lon: 77.2167,
+      lat: 28.6667,
+    },
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d",
+      },
+    ],
+    base: "stations",
+    main: {
+      temp: 31.96,
+      feels_like: 36.39,
+      temp_min: 31.96,
+      temp_max: 32.05,
+      pressure: 1010,
+      humidity: 58,
+      sea_level: 1010,
+      grnd_level: 984,
+    },
+    visibility: 10000,
+    wind: {
+      speed: 4.12,
+      deg: 290,
+    },
+    clouds: {
+      all: 0,
+    },
+    dt: 1789712790,
+    sys: {
+      type: 1,
+      id: 9161,
+      country: "IN",
+      sunrise: 1789691843,
+      sunset: 1789736019,
+    },
+    timezone: 19800,
+    id: 1273294,
+    name: "Delhi",
+    cod: 200,
+  },
+];
